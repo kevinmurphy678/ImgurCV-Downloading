d6d173e365f8c511a9d77631a87f361ef7018278
kevinmurphy678 <kevinmurphy678@gmail.com>
2016-03-27 04:03:20 -0400

    initial

---
+++ puushSpy/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace puushSpy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.debugBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(699, 573);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.numericUpDown2);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 591);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(964, 62);
+            this.panel2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(9, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Clear all images";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(115, 9);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            32,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(44, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // debugBox
+            // 
+            this.debugBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.debugBox.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.debugBox.Location = new System.Drawing.Point(717, 12);
+            this.debugBox.Name = "debugBox";
+            this.debugBox.ReadOnly = true;
+            this.debugBox.Size = new System.Drawing.Size(247, 573);
+            this.debugBox.TabIndex = 6;
+            this.debugBox.Text = "Debug Box";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Get Image(s)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 20);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "DUMP";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(279, 9);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(73, 20);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(165, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 17);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Face Detection";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 653);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.debugBox);
+            this.Name = "Form1";
+            this.Text = "Puush Spy";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.FlowLayoutPanel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox debugBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ puushSpy/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Drawing.Drawing2D;
+
+namespace puushSpy
+{
+    public partial class Form1 : Form
+    {// <summary>Returns true if the current application has focus, false otherwise</summary>
+        public static bool ApplicationIsActivated()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;       // No window is currently activated
+            }
+            var procId = Process.GetCurrentProcess().Id;
+            int activeProcId;
+            GetWindowThreadProcessId(activatedHandle, out activeProcId);
+            return activeProcId == procId;
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+
+        public static int Clamp(int value, int min, int max)
+        {
+            return (value < min) ? min : (value > max) ? max : value;
+        }
+
+        Random random = new Random();
+
+        Form form2;
+
+        public Boolean dumping = false;
+
+
+        private static FaceRecognizer face;
+        public Form1()
+        {
+            InitializeComponent();
+            form2 = new Form2();
+          
+            face = new FisherFaceRecognizer(0, 3500);
+
+        }
+
+        private string RandomString(int Size)
+        {
+            string input = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder builder = new StringBuilder();
+            char ch;
+            for (int i = 0; i < Size; i++)
+            {
+                ch = input[random.Next(0, input.Length)];
+                builder.Append(ch);
+            }
+            return builder.ToString();
+        }
+
+
+        delegate void AddBoxDel(PictureBox box);
+
+        public void AddBox(PictureBox box)
+        {
+            if (panel1.InvokeRequired)
+            {
+                AddBoxDel method = new AddBoxDel(AddBox);
+                panel1.Invoke(method, new object[] { box });
+                return;
+            }
+
+            panel1.Controls.Add(box);
+        }
+
+        delegate void AppendMessageDel(string message);
+
+        public void AppendMessage(string message)
+        {
+            if (debugBox.InvokeRequired)
+            {
+                AppendMessageDel method = new AppendMessageDel(AppendMessage);
+                debugBox.Invoke(method, new object[] { message });
+                return;
+            }
+            debugBox.AppendText(message);
+            debugBox.SelectionStart = debugBox.Text.Length;
+            debugBox.ScrollToCaret();
+        }
+
+
+        int lastx = 0;
+        private void getRandomPuushImage()
+        {
+            Image img=null;
+            string link=null;
+            bool worked = false;
+            String name = null;
+            try
+            {
+                String rand = RandomString(5);
+                link = ("http://www.imgur.com/" + rand + ".jpg");
+                WebRequest req = WebRequest.Create(link);
+                req.Proxy = null;
+                name = rand;
+                WebResponse response = req.GetResponse();
+                
+                if(!response.ResponseUri.AbsoluteUri.ToString().Contains("removed") && !response.ContentType.Contains("gif"))
+                {
+                    Stream stream = req.GetResponse().GetResponseStream();
+                    img = Image.FromStream(stream);
+                    AppendMessage("\n" + link + ": success");
+                    worked = true;
+                    response.Close();
+                }
+                else
+                {
+                    AppendMessage("\n" + link + ": fail");
+                    worked = false;
+                    response.Close();
+                    getRandomPuushImage();
+                }
+
+                response.Close();
+            }
+            catch
+            {
+                AppendMessage("\n" + link + ": fail");
+                getRandomPuushImage();
+            }
+
+            if (worked == true)
+            {
+               
+
+                    Rectangle[] faces=null;
+                    Rectangle[] eyes=null;
+
+                    if (checkBox1.Checked)
+                    {
+                        // Converting the master image to a bitmap
+                        Bitmap masterImage = (Bitmap)img;
+
+                        // Normalizing it to grayscale
+                        Image<Gray, Byte> normalizedMasterImage = new Image<Gray, Byte>(masterImage);
+
+                        float PERCENT = 0.1f;
+                        int xSize = (int)(PERCENT * normalizedMasterImage.Size.Width);
+                        int ySize = (int)(PERCENT * normalizedMasterImage.Size.Height);
+                        int maxSize = Math.Max(xSize, ySize);
+                        CascadeClassifier Classifier = new CascadeClassifier("haarcascade_frontalface_alt2.xml");
+                        faces = Classifier.DetectMultiScale(normalizedMasterImage, 1.03, 10, new Size(maxSize, maxSize), Size.Empty);
+                        CascadeClassifier ClassifierEyes = new CascadeClassifier("haarcascade_eye.xml");
+                        eyes = ClassifierEyes.DetectMultiScale(normalizedMasterImage, 1.03, 10, new Size(maxSize, maxSize), Size.Empty);
+                   }
+       
+
+                if (!dumping)
+                {
+                    if(faces!= null)
+                    foreach (var face in faces)
+                    {
+                         AppendMessage("\nFACE DETECTED!");
+                        using (var graphics = Graphics.FromImage(img))
+                        {
+                                Pen redPen = new Pen(Color.FromArgb(255, 255, 0, 0), 10);
+                                redPen.Alignment = PenAlignment.Center;
+
+                                graphics.DrawRectangle(redPen, face);
+                        }
+                    }
+                    if(eyes!=null)
+                    foreach (var eye in eyes)
+                    {
+                         AppendMessage("\nEYE DETECTED!");
+                        using (var graphics = Graphics.FromImage(img))
+                        {
+                                Pen greenPen = new Pen(Color.FromArgb(255, 0, 255, 0), 10);
+                                greenPen.Alignment = PenAlignment.Center;
+
+                                graphics.DrawRectangle(greenPen, eye);
+                            }
+                    }
+
+                    PictureBox box = new PictureBox();
+                    if (lastx == 512) { lastx = 0; }
+                    box.SetBounds(lastx, 0, 128, 128);
+                    lastx += 128;
+                    box.Image = img;
+                    box.SizeMode = PictureBoxSizeMode.Zoom;
+                    box.Tag = link;
+                    AddBox(box);
+                }
+                else
+                {
+                    if (checkBox1.Checked)
+                    {
+                        if (faces.Length > 0 || eyes.Length > 0)
+                        {
+                            //save image
+                            Console.WriteLine("saving image");
+
+                            img.Save(Directory.GetCurrentDirectory().ToString() + "/images/Faces/" + name + ".jpg");
+
+                        }
+                    }
+
+                    else
+                    {
+                        img.Save(Directory.GetCurrentDirectory().ToString() + "/images/Any/" + name + ".jpg");
+                    }
+                }
+
+                       
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)//get images
+        {
+            dumping = false;
+            for (int i = 0; i < numericUpDown1.Value; i++)
+            {
+                BackgroundWorker imageWorker = new BackgroundWorker();
+                imageWorker.DoWork += new DoWorkEventHandler(imageWorker_DoWork);
+                imageWorker.RunWorkerAsync();     
+            }
+        }
+        private void imageWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            getRandomPuushImage();
+        }
+
+        bool oneclick = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            bool popup = false;
+            foreach (PictureBox box in panel1.Controls)
+            {
+                Point pos = this.PointToClient(Cursor.Position);
+                Rectangle boxrect = new Rectangle(box.Location.X+panel1.Location.X,box.Location.Y+panel1.Location.Y,box.Size.Width,box.Size.Height);
+                bool hovering = boxrect.Contains(pos);
+                bool hoveringForm = this.ClientRectangle.Contains(pos);
+                bool hoveringpanel = panel1.ClientRectangle.Contains(pos);
+                if (hovering&&hoveringForm&&hoveringpanel&&ApplicationIsActivated())
+                {
+                    popup = true;
+                    form2.Show();
+
+                    form2.SetBounds(0, 0, Clamp(box.Image.Width, 0, SystemInformation.PrimaryMonitorSize.Width), Clamp(box.Image.Height, 0, SystemInformation.PrimaryMonitorSize.Height));
+
+                    foreach (PictureBox subbox in form2.Controls)//dunno how to access picturebox1 on form2 directly so im doing this hack
+                    {
+                        subbox.Image = box.Image;
+                        subbox.SizeMode = PictureBoxSizeMode.Zoom;
+                      
+                    }
+                    if ((Control.MouseButtons & MouseButtons.Left) == MouseButtons.Left&&oneclick==false)
+                    {
+                        oneclick = true;
+                        System.Diagnostics.Process.Start((string)box.Tag);
+                      
+                    }
+                    if ((Control.MouseButtons & MouseButtons.Left) != MouseButtons.Left)
+                    {
+                        oneclick = false;
+                    }
+                }
+            }
+            if (popup == false) { form2.Hide(); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//clear images
+        {
+            
+            lastx = 0;
+            ArrayList controls = new ArrayList(panel1.Controls);
+            foreach (Control c in controls)
+            {
+                panel1.Controls.Remove(c);
+                c.Dispose();
+            } 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dumping = true;
+            for (int i = 0; i < numericUpDown2.Value; i++)
+            {
+                BackgroundWorker imageWorker = new BackgroundWorker();
+                imageWorker.DoWork += new DoWorkEventHandler(imageWorker_DoWork);
+                imageWorker.RunWorkerAsync();
+            }
+        }
+    }
+}
